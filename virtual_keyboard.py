c34212ce47ec2c100ccc97debdc4e7f9af6c45dd
leonardoaranhabyjus <leonardo.aranha@byjusfutureschool.com>
2022-04-26 16:15:34 -0300

    init

---
+++ virtual_keyboard.py
@@ -0,0 +1,97 @@
+import cv2
+import mediapipe as mp
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+cap = cv2.VideoCapture(0)
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+
+hands = mp_hands.Hands(min_detection_confidence=0.8, min_tracking_confidence=0.5)
+
+tipIds = [4, 8, 12, 16, 20]
+
+
+# Defina uma função para contar os dedos
+def countFingers(image, hand_landmarks, handNo=0):
+
+    if hand_landmarks:
+        # Obtenha todos os pontos de referência da PRIMEIRA mão VISÍVEL
+        landmarks = hand_landmarks[handNo].landmark
+
+        # Conte os dedos
+        fingers = []
+
+        for lm_index in tipIds:
+                # Obtenha os valores y da ponta e da parte inferior do dedo
+                finger_tip_y = landmarks[lm_index].y 
+                finger_bottom_y = landmarks[lm_index - 2].y
+
+                # Verifique se ALGUM DEDO está ABERTO ou FECHADO
+                if lm_index !=4:
+                    if finger_tip_y < finger_bottom_y:
+                        fingers.append(1)
+                        # print("DEDO com id ",lm_index," está Aberto")
+
+                    if finger_tip_y > finger_bottom_y:
+                        fingers.append(0)
+                        # print("DEDO com id ",lm_index," está Fechado")
+
+        totalFingers = fingers.count(1)
+        
+        # REPRODUZA ou PAUSE um Vídeo
+        
+        ################################
+
+             # ADICIONE O CÓDIGO AQUI  #
+
+        ################################
+        
+
+         # Mova o Vídeo PARA A FRENTE e PARA TRÁS 
+         
+         ################################
+
+             # ADICIONE O CÓDIGO AQUI  #
+
+        ################################ 
+        
+# Defina uma função para 
+def drawHandLanmarks(image, hand_landmarks):
+
+    # Desenhar as conexões entre os pontos de referência
+    if hand_landmarks:
+
+      for landmarks in hand_landmarks:
+               
+        mp_drawing.draw_landmarks(image, landmarks, mp_hands.HAND_CONNECTIONS)
+
+
+
+while True:
+    success, image = cap.read()
+
+    image = cv2.flip(image, 1)
+    
+    # Detecte os pontos de referência das mãos 
+    results = hands.process(image)
+
+    # Obtenha a posição do ponto de referência do resultado processado
+    hand_landmarks = results.multi_hand_landmarks
+
+    # Desenhe os pontos de referência
+    drawHandLanmarks(image, hand_landmarks)
+
+    # Obtenha a posição dos dedos da mão        
+    countFingers(image, hand_landmarks)
+
+    cv2.imshow("Controlador de Midia", image)
+
+    # Saia da tela ao pressionar a barra de espaço
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
+
+cv2.destroyAllWindows()
